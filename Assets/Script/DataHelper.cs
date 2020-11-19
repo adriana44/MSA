740d35810007d93a4c5814b8689209e4adf7ec16
VM <mr_martial22@yahoo.com>
2020-11-19 12:34:25 +0200

    finally starting

---
+++ Assets/Script/DataHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DataHelper : MonoBehaviour
+{
+    public static DataHelper Instance { set; get; }
+    public static BitArray Levels { set; get; }
+    public int CurrentLevel { set; get; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        Update();
+
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Save()
+    {
+        string save = "";
+
+        for (int i = 0; i < Levels.Count; ++i)
+            save += Levels.Get(i).ToString();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
